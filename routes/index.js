6f18df6a123ae8896a0594ebcbcc4cfcc3e7798f
iun007 <654471467@qq.com>
2017-03-01 20:48:45 +0800

    markdown

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var markdown=require('markdown').markdown;
 var models=require('../models');
 var router = express.Router();
 
@@ -7,6 +8,9 @@ router.get('/', function(req, res, next) {
     //user 字符串对象
     //先查找，然后把user字符串转成user对象
     models.Article.find({}).populate('user').exec(function (err,articles) {
+        articles.forEach(function (article) {
+            article.content=markdown.toHTML(article.content)
+        });
         res.render('index', { articles: articles});
     })
 });
