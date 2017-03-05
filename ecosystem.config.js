57a79efd7217019a990360a7f0137c5d9f6ed364
iun007 <654471467@qq.com>
2017-03-05 15:55:19 +0800

    'pm2'

---
+++ ecosystem.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : "API",
+      script    : "./bin/www",
+      env: {
+        COMMON_VARIABLE: "true"
+      },
+      env_production : {
+        NODE_ENV: "production"
+      }
+    },
+
+    // Second application
+    {
+      name      : "WEB",
+      script    : "web.js"
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : "",
+      host : "212.83.163.1",
+      ref  : "origin/master",
+      repo : "https://github.com/iun007/blog.git",
+      path : "/var/www/production",
+      "post-deploy" : "npm install && pm2 startOrRestart ecosystem.config.js --env production"
+    },
+  }
+}
